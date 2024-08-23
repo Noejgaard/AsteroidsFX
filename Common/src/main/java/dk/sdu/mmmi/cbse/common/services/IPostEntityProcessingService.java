767cf9313d582ac48e5ef1a8cb02b235e2c7dc25
Thomas Nojgaard <thomas@nojgaard.dk>
2024-08-23 22:39:25 +0200

    misc

--- Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IPostEntityProcessingService.java
+++ Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IPostEntityProcessingService.java
@@ -3,11 +3,15 @@ package dk.sdu.mmmi.cbse.common.services;
 import dk.sdu.mmmi.cbse.common.data.GameData;
 import dk.sdu.mmmi.cbse.common.data.World;
 
-/**
- *
- * @author jcs
- */
-public interface IPostEntityProcessingService {
 
+public interface IPostEntityProcessingService {
+    /**
+     *
+     *
+     *
+     * @param gameData
+     * @param world
+     * @throws
+     */
     void process(GameData gameData, World world);
 }
--- Player/src/main/java/dk/sdu/mmmi/cbse/playersystem/Player.java
+++ Player/src/main/java/dk/sdu/mmmi/cbse/playersystem/Player.java
@@ -4,7 +4,7 @@ import dk.sdu.mmmi.cbse.common.data.Entity;
 
 /**
  *
- * @author Emil
+ *
  */
 public class Player extends Entity {
     public Player() {
